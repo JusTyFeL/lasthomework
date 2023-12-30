3bb3a7a316097c7217a7fe2895baeff567840e96
JusTyFeL <grikoanton5@gmail.com>
2023-12-30 15:12:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+# ex 1
+
+a = int(input('Введи первое чилос: '))
+b = int(input('Введи второе число: '))
+c = int(input('Введи третье число: '))
+print(f'Сумма чисел равна: {a+b+c}\nПроизведение чисел равна: {a*b*c}')
+
+# ex 2
+
+Salary = int(input('Зарплата за месяц₽: '))
+Payment = int(input('Месячный платёж по кредиту₽: '))
+debt = int(input('Задолжность по коммунальным услугам₽: '))
+print(f'У тебя осталось {Salary - Payment - debt}₽')
+
+# ex 3
+
+Diagonal_1 = int(input('Диогональ площади первой стороны: '))
+Diagonal_2 = int(input('Диогональ площади второй стороны: '))
+
+print(f'Площадь ромба равна: {(Diagonal_1 - Diagonal_2) / 2}')
+
+# ex 4
+
+print(f'To be\nor not\nto be')
+
+# ex 5
+
+print(f"«Life is what happens  \n     when \n         you're busy making other plans» \n                                    John Lennon")
